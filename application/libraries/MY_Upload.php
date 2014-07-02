17a4a12cf6575890789b4777fe7fc778df934f4b
jezard <jez@wizard.technology>
2014-07-02 15:03:53 +0100

    trying function

--- application/libraries/MY_Upload.php
+++ application/libraries/MY_Upload.php
@@ -441,7 +441,7 @@
 							*/
 
 							/*convert fit files*/
-							if($this->file_ext == 'fit')
+							if(/*$this->file_ext == 'fit'*/true == true)
 							{
 								$infile = $this->upload_path.$this->file_name;
 								$outfile = str_replace('.fit', '.tcx', $infile);
