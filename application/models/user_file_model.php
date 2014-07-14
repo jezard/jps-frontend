4238835a234ea5337d5940488725505763f0019e
WizardTechie <jez@wizard.technology>
2014-07-14 16:35:46 +0100

    adding print_r for debug

--- application/models/user_file_model.php
+++ application/models/user_file_model.php
@@ -3,9 +3,11 @@ class User_file_model extends CI_Model{
 	
 	/*associate an uploaded file with its user as a database record*/
 	function linkuser($email, $data){
+		print_r($data);
 		foreach($data as $fileinfo)
 		{
-			echo $fileinfo['file_name'];
+			$filename =  $fileinfo['file_name'];
+
 		}
 
 	}
