026b0b554c3a5c47ea973ee24e6a9bb61f93c532
WizardTechie <jez@wizard.technology>
2014-07-14 16:48:53 +0100

    adding filewrite to db function

--- application/models/user_file_model.php
+++ application/models/user_file_model.php
@@ -3,12 +3,22 @@ class User_file_model extends CI_Model{
 	
 	/*associate an uploaded file with its user as a database record*/
 	function linkuser($email, $data){
-		print_r($data);
 		foreach($data as $fileinfo)
 		{
-			$filename =  $fileinfo['file_name'];
+			$filename =  $fileinfo['raw_name'];
+			$ext = $fileinfo['file_ext'];
+			//swap converted file extensions
+			if($ext == '.fit')
+			{
+				$ext = '.tcx';
+			}
+			$filename = $filename.$ext;
 
+			//add record of file to database
+			$this->email = $email;
+			$this->filename = $filename;
+			$this->filetype = $ext;
+			$this->db->insert('user', $this);
 		}
-
 	}
 }
