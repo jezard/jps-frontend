2a55fc2d7f3165aad62a06c7d90c3bab9ecfdabc
WizardTechie <jez@wizard.technology>
2014-07-14 15:52:45 +0100

    adding short test

--- application/models/user_file_model.php
+++ application/models/user_file_model.php
@@ -4,7 +4,7 @@ class User_file_model extends CI_Model{
 	/*associate an uploaded file with its user as a database record*/
 	function linkuser($email){
 		
-		if ($this->input->cookie('valid_user'))
+		/*if ($this->input->cookie('valid_user'))
 		{
 			$this->email = $this->input->cookie('valid_user', false);
 			$this->filename = $filename;
@@ -14,6 +14,6 @@ class User_file_model extends CI_Model{
 
 			//return id
     		return $this->db->insert_id();
-		}
+		}*/
 	}
 }
