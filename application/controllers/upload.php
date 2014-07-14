bc362f49d741fe237dd70be0c90e507af31dc4d3
WizardTechie <jez@wizard.technology>
2014-07-14 15:35:11 +0100

    adding md5 hash to passwords

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -32,7 +32,7 @@ class Upload extends CI_Controller {
 		));
 
 
-		if (! $this->upload->do_multi_upload("powerfiles",$this->email)) {
+		if (! $this->upload->do_multi_upload("powerfiles",md5($this->email)) {
        		$error = array('error' => $this->upload->display_errors());
        		$this->load->view('templates/header', array('title' => 'Upload - '.$this->config->item('site_name')));
 			$this->load->view('upload_form', $error);
