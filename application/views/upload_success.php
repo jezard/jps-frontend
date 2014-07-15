b793003398eabd2fdf4d52873577ae8c8f22211a
WizardTechie <jez@wizard.technology>
2014-07-15 12:33:04 +0100

    adding processing functionality incrementally

--- application/views/upload_success.php
+++ application/views/upload_success.php
@@ -1,7 +1,16 @@
-
-<h3>Your file was successfully uploaded!</h3>
+<?php
+	$uploadQty = count($fileinfo);
+	$files = 'files were';
+	if($uploadQty == 1) $files = 'file was'; //pet hate of mine using plural word with multiple numeral (1 files etc...)
+?>
+<h3><?php echo $uploadQty.' '.$files.' successfully uploaded!'; ?></h3>
 <ul>
-<?php print_r($fileinfo); ?>
+<?php 
+	foreach($fileinfo as $filemeta)
+	{
+		echo '<li>'.$fileinfo['raw_name'].'</li>'
+	}
+?>
 </ul>
 <p><?php echo anchor('upload', 'Upload more files'); ?></p>
 
