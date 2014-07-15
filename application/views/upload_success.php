b617ce1c64ccf53cb5af7d86c4e47ab2fe2e5f4c
WizardTechie <jez@wizard.technology>
2014-07-15 11:42:35 +0100

    adding processing functionality incrementally

--- application/views/upload_success.php
+++ application/views/upload_success.php
@@ -1,5 +1,9 @@
 
 <h3>Your file was successfully uploaded!</h3>
-<?php echo 'the filename is: '.$filename; ?>
+<ul>
+<?php foreach ($upload_data as $item => $value):?>
+<li><?php echo $item;?>: <?php echo $value;?></li>
+<?php endforeach; ?>
+</ul>
 <p><?php echo anchor('upload', 'Upload more files'); ?></p>
 
