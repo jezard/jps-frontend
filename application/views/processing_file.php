ef7f2e9f9ba178afd0bf3e31ea334670b9766111
WizardTechie <jez@wizard.technology>
2014-07-15 17:04:27 +0100

    adding some processing files. need to plan this out

--- application/views/processing_file.php
+++ application/views/processing_file.php
@@ -1,3 +1,34 @@
 <h3>File processing in progress</h3>
 
+<!-- this is the static outer frame use to house the ajax parse progress window  -->
+<div id="parse-window">
+	<!-- this is the dynamic inner section -->
+	<div id="parse-progress">
+		<!-- ajax content -->
+	</div>
+</div>
+<script type="text/javascript">
+//do a query from the database getting the list of files
+
+//get a count and show number or files to process
+
+//for each file
+
+/*****PHP*******/
+
+//read the contents of the file
+
+//parse contents of file
+
+//write contents of file to database
+
+//delete file
+
+//return done
+
+/******JS******/
+
+//show done
+
+</script>
 <p><?php //echo anchor('upload', 'Upload more files'); ?></p>
