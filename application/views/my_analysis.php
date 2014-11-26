401ce4b1c24f41f938fdb1f9f26c8a7012c47da7
WizardTechie <jez@wizard.technology>
2014-11-26 16:51:41 +0000

    few more visual changes

--- application/views/my_analysis.php
+++ application/views/my_analysis.php
@@ -1,16 +1,17 @@
-<section class="col-1-1">
-<h2>Recent activities</h2>
-<ul class>
-	<?php 
-	foreach ($recentActivities as $activity) {
-		$activityDate = date_create_from_format('Y-m-d H:i:s', $activity['activity_date']);
-		echo '<li><a class="activity-link" href="http://'.$this->config->item('go_ip').'/view/activity/'.$activity['activity_id'].'">'.date_format($activityDate, 'D, jS F Y').'</a></li>';
-	}
-	?>
-</ul>
-</section>
-
-<iframe id="analysis-container" allowTransparency="true" scrolling="no"></iframe>
+<div class="overview-page">
+	<section class="col-1-2">
+	<h2>Recent activities</h2>
+		<ul class>
+			<?php 
+			foreach ($recentActivities as $activity) {
+				$activityDate = date_create_from_format('Y-m-d H:i:s', $activity['activity_date']);
+				echo '<li><a class="activity-link" href="http://'.$this->config->item('go_ip').'/view/activity/'.$activity['activity_id'].'">'.date_format($activityDate, 'D, jS F Y').'</a></li>';
+			}
+			?>
+		</ul>
+	</section>
+	<iframe id="analysis-container" allowTransparency="true" scrolling="no"></iframe>
+</div>
 
 <script>
 jQuery(document).ready(function(){
@@ -19,6 +20,10 @@ jQuery(document).ready(function(){
 		var url = jQuery(this).attr('href');
 		jQuery('#analysis-container').attr('src', url);
 	})
-})
+});
+jQuery('iframe').load(function() {
+    this.style.height =
+    this.contentWindow.document.body.offsetHeight + 'px';
+});
 
 </script>
