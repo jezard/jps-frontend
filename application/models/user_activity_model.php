7b2986132aff16b530afd176b4e9e5f2931c8c85
WizardTechie <jez@wizard.technology>
2015-05-14 10:50:57 +0100

    moved rc4 encoder to helper file

--- application/models/user_activity_model.php
+++ application/models/user_activity_model.php
@@ -14,7 +14,7 @@ class User_activity_model extends CI_Model{
 
 	//TODO
 	function delete($id, $email){
-		$uid = $this->rc4($this->config->item('rc4_cypher'), $email);
+		$uid = rc4($this->config->item('rc4_cypher'), $email);
 		$result = $this->db->query("DELETE FROM user_activity WHERE activity_id = '$id' LIMIT 1");
 		if($result){
 			//also need to send a delete signal to go and create delete functionality there too.
@@ -58,43 +58,6 @@ class User_activity_model extends CI_Model{
 			$data['strava_activity_id'] = $row['strava_activity_id'];
 		}
 		return $data;
-	}
-
-
-
-
-	/*
-	 * RC4 symmetric cipher encryption/decryption
-	 *
-	 * @license Public Domain
-	 * @param string key - secret key for encryption/decryption
-	 * @param string str - string to be encrypted/decrypted
-	 * @return string
-	 */
-	function rc4($key, $str) {
-		$s = array();
-		for ($i = 0; $i < 256; $i++) {
-			$s[$i] = $i;
-		}
-		$j = 0;
-		for ($i = 0; $i < 256; $i++) {
-			$j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
-			$x = $s[$i];
-			$s[$i] = $s[$j];
-			$s[$j] = $x;
-		}
-		$i = 0;
-		$j = 0;
-		$res = '';
-		for ($y = 0; $y < strlen($str); $y++) {
-			$i = ($i + 1) % 256;
-			$j = ($j + $s[$i]) % 256;
-			$x = $s[$i];
-			$s[$i] = $s[$j];
-			$s[$j] = $x;
-			$res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
-		}
-		return $res;
 	}
 
 
