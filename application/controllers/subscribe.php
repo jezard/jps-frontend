8962e276a2c5830e58b7cc7b22d80fc5abea6f23
WizardTechie <jez@wizard.technology>
2015-06-10 15:33:28 +0100

    Adding geoblock for EU countries for VAT MOSS

--- application/controllers/subscribe.php
+++ application/controllers/subscribe.php
@@ -35,20 +35,81 @@ class Subscribe extends CI_Controller {
 		}
 
 		try{
-			$customer = \Stripe\Customer::create(array(
+			/*$customer = \Stripe\Customer::create(array(
 			  "source" => $token,
 			  "plan" => "001",
 			  "email" => $email)
-			);
+			);*/
+			//instead of the above due to the tax issue we need to:
+			//* create the Customer 									https://stripe.com/docs/api/php#create_customer
+			$customer = \Stripe\Customer::create(array(
+			  "description" => "Customer for JoulePerSecond.com",
+			  "email" => $email,
+			  "source" => $token
+			));
+			
+			//* get the newly created cus_id
+			//* get the card for the user 								https://stripe.com/docs/api/php#retrieve_card
+			$card = $customer->sources->retrieve($customer->default_source);
+			$country_code = $card->country;
+
+			//TODO check against array of country codes
+			$countries = array(
+							  "AT" => "Austria",
+							  "BE" => "Belgium",
+							  "BG" => "Bulgaria",
+							  "HR" => "Croatia",
+							  "CU" => "Cuba",
+							  "CY" => "Cyprus",
+							  "CZ" => "Czech Republic",
+							  "DK" => "Denmark",
+							  "EE" => "Estonia",
+							  "FI" => "Finland",
+							  "FR" => "France",
+							  "DE" => "Germany",
+							  "GR" => "Greece",
+							  "HU" => "Hungary",
+							  "IE" => "Ireland",
+							  "IT" => "Italy",
+							  "LV" => "Latvia",
+							  "LT" => "Lithuania",
+							  "LU" => "Luxembourg",
+							  "MT" => "Malta",
+							  "NL" => "Netherlands",
+							  "PL" => "Poland",
+							  "PT" => "Portugal",
+							  "RO" => "Romania",
+							  "SK" => "Slovakia",
+							  "SI" => "Slovenia",
+							  "ES" => "Spain",
+							  "SE" => "Sweden"
+							);
+			foreach ($countries as $key => $value) {
+				if($country_code == $key)
+				{
+					echo "Unfortunately, due to VAT MOSS regulations we are unable to supply our services to ".$value;
+					return;
+				}
+			}
+
+			//* if not uk, us, aus, nz, canada etc. then reject else 	
+			//* create a subscription 									https://stripe.com/docs/api/php#create_subscription
+			$customer->subscriptions->create(array("plan" => "001"));
+
+			//* probably better store the country code etc...
+
 		}catch(Exception $e){
 			echo 'Unfortunately, You have not been subscribed: ',  $e->getMessage(), "\n";
+			return;
 		}
 		
 
-		$this->user->set_stripe_id($email, $customer->id);
+		$this->user->set_stripe_id($email, $customer->id, $country_code);
 
 		if($this->user->set_as_subscriber($email)){
 			echo 'Thank you very much. You are now subscribed!';
+		}else{
+			echo 'There was an unknown problem :(';
 		}
 	}
 }
