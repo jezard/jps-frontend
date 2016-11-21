860ef1bdc69f970673227e201ebd01c048e6d3af
Jeremy Summers <jez@wizard.technology>
2016-11-21 20:50:10 +0000

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,7 +41,7 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
-//$route['index.php/upload'] = "upload";
+$route['index.php/upload'] = "upload";
 
 
 /* End of file routes.php */
